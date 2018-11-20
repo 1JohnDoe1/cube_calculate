81adde668b71b25b4f15528b1befbe18f48c6c96
1JohnDoe1 <45199537+1JohnDoe1@users.noreply.github.com>
2018-11-20 19:27:35 +0700

    Update cube.cpp

--- cube.cpp
+++ cube.cpp
@@ -1,13 +1,37 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+int correctValue(int, int);
+int cube(int);
 int main() {
-   
    int x = 0;
-   scanf("%d", &x);
-   
-   x = x * x * x;
-   
-   printf("%d", x);
-   
+   int y = 0;
+   printf("Введите число: ");
+   x = correctValue(1, 100);
+   y = cube(x);
+   printf("Результат: %d", y);
    return 0;
+}
+int cube(int number)
+{
+    int x = 0;
+    x = number * number * number;
+    return x;
+}
+int correctValue(int min, int max)
+{
+    int value = 0;
+    char enter = '\0';
+    do
+    {
+        while (scanf("%d%c", &value, &enter, 1) != 2 || enter != '\n')
+        {
+            printf("Ошибка! Введите только целые положительные числа: ");
+            while (getchar() != '\n');
+        }
+        if (value < min || value > max)
+        {
+            printf("Ошибка! Введите значения от %d до %d: ", min, max);
+        }
+    } while (value < min || value > max);
+    return value;
 }
